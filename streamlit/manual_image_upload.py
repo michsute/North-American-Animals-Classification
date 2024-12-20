346d61caf86a644d3c48a885df2bc0b6d9966fa3
nadeeiiin <nadine_flueck@hotmail.com>
2024-12-20 11:38:30 +0100

    manual image uploader + example classifier works

--- streamlit/manual_image_upload.py
+++ streamlit/manual_image_upload.py
@@ -1,46 +1,21 @@
 import streamlit as st
 from PIL import Image
-from torchvision import models, transforms
-import torch
+from clf_example_model import predict
 
 def show_manual_image_upload():
     
+    #st.set_option('deprecation.showfileUploaderEncoding', False)
+
     #image uploader
     file_up = st.file_uploader("Upload an image", type="jpg")
 
-    image = Image.open(file_up)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    if file_up is not None:
+        image = Image.open(file_up)
+        st.image(image, caption='Uploaded Image.', use_container_width=True)
+        st.write("")
+        st.write("Just a second...")
+        labels = predict(file_up)
 
-
-    # EXAMPLE CHANGE TO OUR MODEL
-    resnet = models.resnet101(pretrained=True)
-
-    transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
-    )])
-
-    # load the image, pre-process it, and make predictions
-    img = Image.open(image_path)
-    batch_t = torch.unsqueeze(transform(img), 0)
-    resnet.eval()
-    out = resnet(batch_t)
-
-    # return the top 5 predictions ranked by highest probabilities
-    prob = torch.nn.functional.softmax(out, dim=1)[0] * 100
-    _, indices = torch.sort(out, descending=True)
-    return [(classes[idx], prob[idx].item()) for idx in indices[0][:5]]
-
-
-
-
-
-
-
-
-
-
+        # print out the top 5 prediction labels with scores
+        for i in labels:
+            st.write("Prediction (index, name)", i[0], ",   Score: ", i[1])
